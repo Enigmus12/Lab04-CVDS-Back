f59d8709d898fe6f9659f7f357a4edc9b490a4b5
Esteban Aguilera <estebanac0903@gmail.com>
2025-02-18 20:24:26 -0500

    Creando estructura (scaffolding) del proyecto

---
+++ src/test/java/edu/eci/cvds/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
